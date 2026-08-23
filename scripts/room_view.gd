b669367810b431d6400d3cd0f24c2252c8701334
Finn T. <81704680+98whiskers@users.noreply.github.com>
2026-08-23 10:10:44 +0200

    Add basic level change logic

--- scripts/room_view.gd
+++ scripts/room_view.gd
@@ -6,3 +6,17 @@ func _ready():
 	var current_data: LevelData = GameState.get_current_level()
 	level_generator.order = current_data.order
 	level_generator.generate_level()
+
+func _on_finish_level_pressed() -> void:
+	_evaluate_level()
+	
+	if GameState.is_last_level():
+		# mit spiel beendet screen ersetzen?
+		get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
+	else:
+		GameState.advance_level()
+		get_tree().change_scene_to_file("res://scenes/roomView.tscn") # beim neuladen wird die level-data aktualisiert mit dem nächsten level
+
+func _evaluate_level():
+	# hier bitte die score logik einbinden
+	pass
---
+++ scripts/start_menu.gd
@@ -0,0 +1,6 @@
+extends Control
+
+func _on_start_button_pressed():
+	GameState.current_level_index = 0
+	# get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/roomView.tscn")
