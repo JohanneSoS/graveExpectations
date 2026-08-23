fad4564ba5ae0b40b8d14d3a500154be65b05032
Alex-Semina <a.tannenberg@outlook.com>
2026-08-23 15:14:57 +0200

    added quit game button and func to button

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -4,3 +4,7 @@ func _on_start_button_pressed():
 	GameState.current_level_index = 0
 	# get_tree().change_scene_to_file("res://scenes/boss_room.tscn")
 	get_tree().change_scene_to_file("res://scenes/boss_room.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
